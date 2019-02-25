12ef919378fb738f9b0218c1f995fe0a9ea53a49
aestefam <47981990+aestefam@users.noreply.github.com>
2019-02-25 12:29:25 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,14 +1,29 @@
-'use strict'        // let the browser know we're serious
+'use strict'
 
-// debug statement letting us know the file is loaded
 console.log('Loaded map.js')
 
-// your mapbox token
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWVzdGVmYW0iLCJhIjoiY2pza2dzeTF4MTYzOTQ5bnpyZm96djd3dSJ9.fwglwbY2j0zooLW4jR0zlw'
+mapboxgl.accessToken = 'YOUR TOKEN HERE BETWEEN THE QUOTES'
+
 let map = new mapboxgl.Map({
     container: 'map',
-    style:'mapbox://styles/brianhouse/cjn0u552b52kr2spdz6yhpqj4',
-    center: [-22.923103,-43.252815],
-    zoom: 16,
-    pitch: 45
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+    zoom: 12
 })
+
+// create an instance of NavigationControl
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+
+// add the navigation to your map
+map.addControl(navigation, 'top-left')
+
+// create an instance of ScaleControl
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+
+// add the scale to your map
+map.addControl(scale, 'bottom-right')
