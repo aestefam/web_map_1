0f47c6089daf68f4a4114f504befbced1328b38e
aestefam <47981990+aestefam@users.noreply.github.com>
2019-02-25 11:51:41 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!")
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWVzdGVmYW0iLCJhIjoiY2pza2dzeTF4MTYzOTQ5bnpyZm96djd3dSJ9.fwglwbY2j0zooLW4jR0zlw'
