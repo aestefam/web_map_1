45230c80baf44ac19cad58713bbc2c6a367cfce2
aestefam <47981990+aestefam@users.noreply.github.com>
2019-02-25 12:30:11 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,29 +1,6 @@
-'use strict'
-
-console.log('Loaded map.js')
-
-mapboxgl.accessToken = 'YOUR TOKEN HERE BETWEEN THE QUOTES'
-
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
     center: [-73.96024, 40.80877],
-    zoom: 12
+    zoom: 1
 })
-
-// create an instance of NavigationControl
-let navigation = new mapboxgl.NavigationControl({
-    showCompass: false
-})
-
-// add the navigation to your map
-map.addControl(navigation, 'top-left')
-
-// create an instance of ScaleControl
-let scale = new mapboxgl.ScaleControl({
-    maxWidth: 80,
-    unit: 'imperial'
-})
-
-// add the scale to your map
-map.addControl(scale, 'bottom-right')
