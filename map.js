ac39df396bde07d85fa984281024e53ef952a5d5
aestefam <47981990+aestefam@users.noreply.github.com>
2019-02-25 12:13:36 -0500

    Update map.js

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ console.log('Loaded map.js')
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWVzdGVmYW0iLCJhIjoiY2pza2dzeTF4MTYzOTQ5bnpyZm96djd3dSJ9.fwglwbY2j0zooLW4jR0zlw'
 let map = new mapboxgl.Map({
     container: 'map',
-    style:'mapbox://styles/mapbox/navigation-preview-night-v2',
+    style:'mapbox://styles/mapbox/navigation-preview-day-v2',
     center: [-73.96024, 40.80877],
     zoom: 1
 })
