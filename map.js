28b6c7df32621ed7a1eb036c34b2efb769595af9
aestefam <47981990+aestefam@users.noreply.github.com>
2019-02-25 12:32:46 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-73.96024, 40.80877],
-    zoom: 1
-})
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWVzdGVmYW0iLCJhIjoiY2pza2dzeTF4MTYzOTQ5bnpyZm96djd3dSJ9.fwglwbY2j0zooLW4jR0zlw'
